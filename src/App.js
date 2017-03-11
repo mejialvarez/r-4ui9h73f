6922500ca47a96dd18da894e6e483d9ea4eba856
Hamilton Alvarez Mejia <alvarezhamilton@gmail.com>
2017-03-11 15:47:54 -0500

    Adds updateRepeater method

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			repeater: ""
+		}
+	}
+
+	updateRepeater(event) {
+		this.setState({
+			repeater: event.target.value
+		});
+	}
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={this.updateRepeater.bind(this)} />
+        <p className="repeater">{this.state.repeater}</p>
       </div>
     );
   }
